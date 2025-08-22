a04c1e2797d973a06ea640842a3e284ceba04f23
uNdoNe001 <147772862+uNdoNe001@users.noreply.github.com>
2025-08-22 15:29:34 -0600

    Create next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: '/api/:path*', destination: 'http://backend:8000/api/:path*' },
+    ];
+  },
+};
+module.exports = nextConfig;
